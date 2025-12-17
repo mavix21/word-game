e3ff7cbeb8f27867dbc43060657dc7d6930b99d4
mavix21 <marcevizcarra2109@hotmail.com>
2025-12-16 19:06:20 -0500

    feat: implement rendering of guesses in GuessHistoryList component

--- src/components/GuessHistoryList/GuessHistoryList.js
+++ src/components/GuessHistoryList/GuessHistoryList.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function GuessHistoryList({ guesses }) {
-  return <div></div>;
+  return (
+    <div class="guess-results">
+      {guesses.map((guess, index) => (
+        <p key={index} class="guess">
+          {guess}
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default GuessHistoryList;
