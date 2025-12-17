9a4b2fdb028b7073cc768e945eb44fe70a41ecd7
mavix21 <marcevizcarra2109@hotmail.com>
2025-12-16 19:31:35 -0500

    feat: implement Game component to manage game state and render guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { sample } from "../../utils";
+import { range, sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
+import Guess from "../Guess/Guess";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,11 +13,27 @@ console.info({ answer });
 
 function Game() {
   const [guess, setGuess] = React.useState("");
+  const [guessHistory, setGuessHistory] = React.useState([]);
+
+  const addToGuessHistory = React.useCallback((guess) => {
+    setGuessHistory((prev) => [...prev, guess]);
+  }, []);
 
   return (
-    <>
-      <GuessInput guess={guess} setGuess={setGuess} />
-    </>
+    <div className="wrapper">
+      <div class="game-wrapper">
+        <div className="guess-results">
+          {range(NUM_OF_GUESSES_ALLOWED).map((n, i) => (
+            <Guess key={n} guess={guessHistory[i]} answer={answer} />
+          ))}
+        </div>
+        <GuessInput
+          guess={guess}
+          setGuess={setGuess}
+          onSubmit={addToGuessHistory}
+        />
+      </div>
+    </div>
   );
 }
 
